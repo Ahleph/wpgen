502e3dbea22afd6e7a5d84f291ec16e07af069cb
aleeph <aleeph4@gmail.com>
2014-12-12 23:14:55 -0500

    fixed to work in python 3.x

--- src/s442.py
+++ src/s442.py
@@ -12,8 +12,8 @@ def main():
 ## Take top half triangle and copy it over the hypotenuse. gives one reflection:
 ## Specifically, the reflection of order 2
 def HypReflect(picture, pic, width, height):
-  for i in xrange(height):
-    for j in xrange(width - i):
+  for i in range(height):
+    for j in range(width - i):
         pic[width-i-1, height-j-1] = pic[j,i]
 
   picture.save("new2.png")
@@ -33,8 +33,8 @@ def DoubleWallpaperSize(pic, width, height):
   newimg = newim.load()
   im = Image.open("new2.png")
   img = im.load()
-  for i in xrange(height/2):
-    for j in xrange(width/2):
+  for i in range(int(height/2)):
+    for j in range(int(width/2)):
       newimg[i,j] = img[i,j]
   newim = HorizontalReflect(newim, newimg, width, height)
   newimg = newim.load()
@@ -42,15 +42,15 @@ def DoubleWallpaperSize(pic, width, height):
   return (newim, width * 2, height * 2)
 
 def HorizontalReflect(picture, pic, width, height):
-  for i in xrange(height - 1):
-      for j in xrange(width/2 - 1):
+  for i in range(height - 1):
+      for j in range(int(width/2 - 1)):
           pic[width - j - 1, i] = pic[j, i]
   picture.save("new2.png")
   return Image.open("new2.png")
 
 def VerticalReflect(picture, pic, width, height):
-  for i in xrange(height/2 - 1):
-      for j in xrange(width - 1):
+  for i in range(int(height/2 - 1)):
+      for j in range(width - 1):
           pic[j , height - i - 1] = pic[j, i]
   picture.save("new2.png")
 
